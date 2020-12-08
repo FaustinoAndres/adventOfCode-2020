ba757a98c38ce30740fa51cee903a241924a21c9
Faustino Correa <faustino.correa.m@gmail.com>
2020-12-08 13:40:57 -0300

    Finish Day1 problem 1

--- day1_p1.py
+++ day1_p1.py
@@ -1,9 +1,27 @@
 
+def readFile():
+
+    f = open("day1_p1_data.txt", "r")
+    lines = f.readlines()
+    input_numbers = list()
+    for line in lines:
+        input_numbers.append(int(line[:-1]))
+    return input_numbers
 
 def main():
 
-    pass
+    objective = 2020
+    input_numbers = readFile()
+    complement = dict()
 
+    for number in input_numbers:
+        complement[number] = True
+        to_find = objective - number
+
+        if to_find in complement:
+
+            print(to_find*number)
+            break
 
 if __name__ == '__main__':
 
